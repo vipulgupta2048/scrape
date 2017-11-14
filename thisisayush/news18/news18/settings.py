0a7db75d6d1b909b701e8702d184058a95ab8505
Ayush Agarwal <thisisayushaa@gmail.com>
2017-11-14 15:21:47 +0530

    Add Tor Integration

--- thisisayush/news18/news18/settings.py
+++ thisisayush/news18/news18/settings.py
@@ -28,13 +28,13 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.25
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -53,9 +53,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'news18.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+HTTP_PROXY = 'http://127.0.0.1:8123'
+DOWNLOADER_MIDDLEWARES = {
+    'news18.middlewares.TorProxyMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
