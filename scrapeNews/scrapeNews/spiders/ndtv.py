73dfd6b92dd6309b38efdfc0ea9ae0fefe9685c6
Parth Sharma <parth261297@gmail.com>
2017-12-01 01:04:59 +0530

    Fixed date extraction

--- scrapeNews/scrapeNews/spiders/ndtv.py
+++ scrapeNews/scrapeNews/spiders/ndtv.py
@@ -32,9 +32,6 @@ class NdtvSpider(scrapy.Spider):
                     item['title'] = news.css('div.nstory_header>a::text').extract_first().strip()
                     item['content'] = news.css('div.nstory_intro::text').extract_first()
                     item['link'] = news.css('div.nstory_header>a::attr(href)').extract_first()
-                    #item['newsDate'] = news.css('div.nstory_dateline::text').extract_first().strip()[2:28]
-                    #item['newsDate'] = yield
-                    #item['newsDate'] = (yield scrapy.Request(item['link'], callback=self.parse_date))['date']
                     item['newsDate'] = self.parse_date(item['link'])
                     item['source'] = 104
                     yield item
@@ -42,17 +39,20 @@ class NdtvSpider(scrapy.Spider):
                     logging.debug('Skipping a News Item, most probably an Advertisment')
 
     def parse_date(self, link):
-        #response = scrapy.http.Response(link)
-        #r = scrapy.http.HtmlResponse(link)
         page = requests.get(link)
         r = html.fromstring(page.content)
         if r.xpath('//span[@itemprop="dateModified"]/@content'):
             return r.xpath('//span[@itemprop="dateModified"]/@content')[0][:-6]
+        elif r.xpath('//meta[@name="publish-date"]/@content'):
+            try :
+                date = r.xpath('//meta[@name="publish-date"]/@content')[0][:-6]
+                date = (datetime.strptime(date, '%a, %d %b %Y %H:%M:%S')).strftime("%Y-%m-%dT%H:%M:%S")
+            except ValueError:
+                date = r.xpath('//meta[@name="publish-date"]/@content')[0][:-6]
+                date = (datetime.strptime(date, '%a,%d %b %Y %H:%M:%S')).strftime("%Y-%m-%dT%H:%M:%S")
+            return date
         elif r.xpath('//meta[@itemprop="dateModified"]/@content'):
             return r.xpath('//meta[@itemprop="dateModified"]/@content')[0][:-6]
-        elif r.xpath('//meta[@name="publish-date"]/@content'):
-            date = r.xpath('//meta[@name="publish-date"]/@content')[0][:-6]
-            return (datetime.strptime(date, '%a, %d %b %Y %H:%M:%S')).strftime("%Y-%m-%dT%H:%M:%S")
         else :
             logging.critical('ADD THIS: ' + link)
             return None
