137aea0e8b8e919d0f60f325a2475101392b7d03
Ayush Agarwal <thisisayushaa@gmail.com>
2017-12-08 17:30:02 +0530

    (add) [Logging] Hindustan Spider

--- scrapeNews/scrapeNews/spiders/hindustan.py
+++ scrapeNews/scrapeNews/spiders/hindustan.py
@@ -2,6 +2,7 @@
 import scrapy
 from scrapeNews.items import ScrapenewsItem
 from scrapeNews.db import DatabaseManager, LogsManager
+from scrapeNews.settings import logger
 
 class HindustanSpider(scrapy.Spider):
 
@@ -21,31 +22,36 @@ class HindustanSpider(scrapy.Spider):
     start_urls = [temp+'1']
     count = 1
     def parse(self, response):
-        if len(response.text) != 0:
-            something = response.css('div.media')
-            for somethings in something:
-                link = somethings.xpath('.//div[contains(@class,"media-body")]/div[1]/a/@href').extract_first()
-                if not DatabaseManager().urlExists(link):
-                    self.custom_settings['url_stats']['parsed'] += 1
-                    yield scrapy.Request(url=link, callback=self.fun)
-                else:
-                    self.custom_settings['url_stats']['dropped'] += 1
-            self.count += 1
-            #print(self.count)
-            yield scrapy.Request(url=self.temp+str(self.count), callback=self.parse)
+        try:
+            if len(response.text) != 0:
+                something = response.css('div.media')
+                for somethings in something:
+                    link = somethings.xpath('.//div[contains(@class,"media-body")]/div[1]/a/@href').extract_first()
+                    if not DatabaseManager().urlExists(link):
+                        self.custom_settings['url_stats']['parsed'] += 1
+                        yield scrapy.Request(url=link, callback=self.fun)
+                    else:
+                        self.custom_settings['url_stats']['dropped'] += 1
+                self.count += 1
+                #print(self.count)
+                yield scrapy.Request(url=self.temp+str(self.count), callback=self.parse)
+       except Exception as e:
+           logger.error(__name__ + " Unhandled: " + str(e))
 
     def fun(self, response):
-        text = response.text
-        headline = response.xpath('//h1[@itemprop="headline"]/text()').extract_first()
-        images = response.xpath('//div[@class="thumbnail"]/img/@src').extract_first()
-        body = response.xpath('//div[@itemprop="articlebody"]/p').extract_first()
-        date = response.css('span.text-dt::text').extract_first()
-        
-        item = ScrapenewsItem({'title': headline, 'link': response.url, 'newsDate': date, 'content': body, 'image': images})
+        try:
+            text = response.text
+            headline = response.xpath('//h1[@itemprop="headline"]/text()').extract_first()
+            images = response.xpath('//div[@class="thumbnail"]/img/@src').extract_first()
+            body = response.xpath('//div[@itemprop="articlebody"]/p').extract_first()
+            date = response.css('span.text-dt::text').extract_first()
+            item = ScrapenewsItem({'title': headline, 'link': response.url, 'newsDate': date, 'content': body, 'image': images})
 
-        self.custom_settings['url_stats']['scraped'] += 1
-        yield item
-    
+            self.custom_settings['url_stats']['scraped'] += 1
+            yield item
+        except Exception as e:
+            logger.error(__name__ + " Unhandled: " + str(e))
+
     def closed(self, reason):
         LogsManager().end_log(self.custom_settings['log_id'], self.custom_settings['url_stats'], reason)
 
