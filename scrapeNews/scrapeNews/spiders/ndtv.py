89d3824873cef849ae64eedc7a28533bafb7dcbe
Parth Sharma <parth261297@gmail.com>
2017-12-14 20:44:43 +0530

    A few minor changes

--- scrapeNews/scrapeNews/spiders/ndtv.py
+++ scrapeNews/scrapeNews/spiders/ndtv.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapeNews.items import ScrapenewsItem
-import time
-import logging
+from scrapeNews.pipelines import loggerError
 import requests
 from lxml import html
 from datetime import datetime
@@ -10,44 +9,42 @@ from datetime import datetime
 
 class NdtvSpider(scrapy.Spider):
 
-
     name = 'ndtv'
     start_urls = ['http://www.ndtv.com/latest/']
     custom_settings = {
-        'site_id':104,
-        'site_name':'NDTV',
-        'site_url':'https://www.ndtv.com/latest/page-1'}
-
+        'site_id': 104,
+        'site_name': 'NDTV',
+        'site_url': 'https://www.ndtv.com/latest/page-1'}
+    download_delay = 2
 
     def __init__(self, pages=1, *args, **kwargs):
         super(NdtvSpider, self).__init__(*args, **kwargs)
         self.pages = pages
+        for count in range(0, int(pages)):
+            self.start_urls.append('http://www.ndtv.com/latest/page-' + str(count + 1))
 
     def closed(self, reason):
         self.postgres.closeConnection(reason)
 
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, callback=self.parse_news, errback=self.errorRequestHandler)
 
-    def parse(self, response):
-        page_ctr = 1
-        while page_ctr <= int(self.pages):
-            time.sleep(2)
-            next_page = 'http://www.ndtv.com/latest/page-' + str(page_ctr)
-            yield scrapy.Request(next_page, callback=self.parse_news)
-            page_ctr += 1
+    def errorRequestHandler(self, failure):
+        self.urls_parsed -= 1
+        loggerError.error('ERROR: ' + str(failure.request.url))
 
     def parse_news(self, response):
         item = ScrapenewsItem()  # Scraper Items
-        for news in response.css('div.new_storylising>ul>li'):
-                if news.css('div.nstory_header>a::text'):
-                    item['image'] = news.css('div.new_storylising_img>a>img::attr(src)').extract_first()
-                    item['title'] = news.css('div.nstory_header>a::text').extract_first().strip()
-                    item['content'] = news.css('div.nstory_intro::text').extract_first()
-                    item['link'] = news.css('div.nstory_header>a::attr(href)').extract_first()
+        for news in response.xpath('//div[@class="new_storylising"]/ul/li'):
+                if news.xpath('.//div[@class="nstory_header"]/a/text()'):
+                    item['image'] = news.xpath('.//img/@src').extract_first()
+                    item['title'] = news.xpath('.//div[@class="nstory_header"]/a/text()').extract_first().strip()
+                    item['content'] = news.xpath('.//div[@class="nstory_intro"]/text()').extract_first()
+                    item['link'] = news.xpath('.//div[@class="nstory_header"]/a/@href').extract_first()
                     item['newsDate'] = self.parse_date(item['link'])
                     item['source'] = 104
                     yield item
-                else:
-                    logging.debug('Skipping a News Item, most probably an Advertisment')
 
     def parse_date(self, link):
         page = requests.get(link)
@@ -65,5 +62,5 @@ class NdtvSpider(scrapy.Spider):
         elif r.xpath('//meta[@itemprop="dateModified"]/@content'):
             return r.xpath('//meta[@itemprop="dateModified"]/@content')[0][:-6]
         else :
-            logging.critical('ADD THIS: ' + link)
+            loggerError.error('NDTV ADD THIS: ' + link)
             return None
