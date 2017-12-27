697f9bad7d81435edd44958c80ffd9382e9a571f
Ayush Agarwal <thisisayushaa@gmail.com>
2017-12-27 16:58:01 +0530

    Add Tor Support

--- scrapeNews/scrapeNews/settings.py
+++ scrapeNews/scrapeNews/settings.py
@@ -52,9 +52,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'scrapeNews.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+HTTP_PROXY = 'http://127.0.0.1:8118'
+DOWNLOADER_MIDDLEWARES = {
+    'scrapeNews.middlewares.TorProxyMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
