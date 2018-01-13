c07c8d97a0484e394eac0e6d94a1831021255539
Ayush Agarwal <thisisayushaa@gmail.com>
2018-01-13 14:28:30 +0530

    (firstpostSports) Improve Logging and Error Handling

--- scrapeNews/scrapeNews/spiders/firstpostSports.py
+++ scrapeNews/scrapeNews/spiders/firstpostSports.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapeNews.items import ScrapenewsItem
-from scrapeNews.pipelines import loggerError
+from scrapeNews.settings import logger
 
 
 class FirstpostsportsSpider(scrapy.Spider):
@@ -23,30 +23,41 @@ class FirstpostsportsSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse, errback=self.errorRequestHandler)
 
     def errorRequestHandler(self, failure):
-        self.urls_parsed -= 1
-        loggerError.error('Non-200 response at ' + str(failure.request.url))
-
+        logger.error(__name__ + ' Non-200 response at ' + str(failure.request.url))
 
     def parse(self, response):
-        newsContainer = response.xpath("//ul[@class='articles-list']/li")
-        for newsBox in newsContainer:
-            link = newsBox.xpath('a/@href').extract_first()
-            if not self.postgres.checkUrlExists(link):
-                yield scrapy.Request(url=link, callback=self.parse_article, errback=self.errorRequestHandler)
+        try:
+            newsContainer = response.xpath("//ul[@class='articles-list']/li")
+            for newsBox in newsContainer:
+                link = newsBox.xpath('a/@href').extract_first()
+                if not self.postgres.checkUrlExists(link):
+                    self.urls_parsed += 1
+                    yield scrapy.Request(url=link, callback=self.parse_article, errback=self.errorRequestHandler)
+                else:
+                    self.urls_dropped += 1
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] " + str(e) + " for response url " + response.url)
 
 
     def parse_article(self, response):
-        if (str(response.url)[:32] != "http://www.firstpost.com/photos/"):
-            item = ScrapenewsItem()  # Scraper Items
-            item['image'] = self.getPageImage(response)
-            item['title'] = self.getPageTitle(response)
-            item['content'] = self.getPageContent(response)
-            item['newsDate'] = self.getPageDate(response)
-            item['link'] = response.url
-            item['source'] = 112
-            if item['title'] is not 'Error' and item['content'] is not 'Error' and item['link'] is not 'Error' and item['newsDate'] is not 'Error':
-                self.urls_scraped += 1
-                yield item
+        try:
+            if (str(response.url)[:32] != "http://www.firstpost.com/photos/"):
+                item = ScrapenewsItem()  # Scraper Items
+                item['image'] = self.getPageImage(response)
+                item['title'] = self.getPageTitle(response)
+                item['content'] = self.getPageContent(response)
+                item['newsDate'] = self.getPageDate(response)
+                item['link'] = response.url
+                item['source'] = 112
+                if item['title'] is not 'Error' and item['content'] is not 'Error' and item['link'] is not 'Error' and item['newsDate'] is not 'Error':
+                    self.urls_scraped += 1
+                    yield item
+                else:
+                    self.urls_dropped += 1
+                    yield None
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] " + str(e) + " for response url " + response.url)
+            self.urls_dropped += 1
 
 
     def getPageTitle(self, response):
@@ -57,18 +68,22 @@ class FirstpostsportsSpider(scrapy.Spider):
             if (data is None):
                 data = response.xpath('//h1[@class="page-title article-title"]/text()').extract_first()
             if (data is None):
-                loggerError.error(Error, response.url)
+                logger.error(__name__ + " Unable to Extract Title " + str(Error) + " : " response.url)
                 data = 'Error'
         except Exception as Error:
-            loggerError.error(Error, response.url)
+            logger.error(__name__ + " Unable to Extract Title " + str(Error) + " : " response.url)
             data = 'Error'
-        finally:
-            return data
+        
+        return data
 
     def getPageImage(self, response):
-        data = response.xpath("/html/head/meta[@property='og:image']/@content").extract_first()
-        if (data is None):
-            loggerError.error(response.url)
+        try:
+            data = response.xpath("/html/head/meta[@property='og:image']/@content").extract_first()
+            if (data is None):
+                logger.error(__name__ + " Unable to Extract Title : " response.url)
+                data = 'Error'
+        except Exception as e:
+            logger.error(__name__ + " Unable to Extract Image " + str(e) + " : " response.url)
             data = 'Error'
         return data
 
@@ -77,26 +92,29 @@ class FirstpostsportsSpider(scrapy.Spider):
             # split & rsplit Used to Spit Data in Correct format!
             data = (response.xpath("//head/meta[@property='article:published_time']/@content").extract_first()).rsplit('+',1)[0]
         except Exception as Error:
-            loggerError.error(Error, response.url)
+            logger.error(__name__ + " Unable to Extract Date " + str(Error) + " : " response.url)
             data = 'Error'
-        finally:
-            return data
+        return data
 
     def getPageContent(self, response):
-        data = ' '.join(response.xpath("//div[contains(@class,'article-full-content')]/p//text()").extract())
-        if not data:
-            data = ' '.join(response.xpath("//div[contains(@class,'article-full-content')]/div/p/text()").extract())
-        if not data:
-            data  =' '.join(response.xpath("//div[contains(@class,'article-full-content')]/div[not(@class)]//text()").extract())
-        if not data:
-            data = ' '.join(response.xpath("//div[contains(@class,'article')]/p[not(@class) and not(@id)]/text()").extract())
-        if not data:
-            data = ' '.join(response.xpath("//div[@itemprop='articleBody']/p/span/text()").extract())
-        if not data:
-            data = ' '.join(response.xpath("//div[@itemprop='articleBody']/div[not(@class)]//text()").extract())
-        if not data:
-            data = ' '.join(response.xpath("//div[@class='consumption']/p//text()").extract())
-        if not data:
-            loggerError.error(response.url)
+        try:
+            data = ' '.join(response.xpath("//div[contains(@class,'article-full-content')]/p//text()").extract())
+            if not data:
+                data = ' '.join(response.xpath("//div[contains(@class,'article-full-content')]/div/p/text()").extract())
+            if not data:
+                data  =' '.join(response.xpath("//div[contains(@class,'article-full-content')]/div[not(@class)]//text()").extract())
+            if not data:
+                data = ' '.join(response.xpath("//div[contains(@class,'article')]/p[not(@class) and not(@id)]/text()").extract())
+            if not data:
+                data = ' '.join(response.xpath("//div[@itemprop='articleBody']/p/span/text()").extract())
+            if not data:
+                data = ' '.join(response.xpath("//div[@itemprop='articleBody']/div[not(@class)]//text()").extract())
+            if not data:
+                data = ' '.join(response.xpath("//div[@class='consumption']/p//text()").extract())
+            if not data:
+                logger.error(__name__ + " Unable to Extract Content : " response.url)
+                data = 'Error'
+        except Exception as e:
+            logger.error(__name__ + " Unable to Extract Content " + str(e) + " : " response.url)
             data = 'Error'
         return data
