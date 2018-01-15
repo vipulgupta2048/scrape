6a902bd900c2653a2d04d083135d6fc950bf9910
Ayush Agarwal <thisisayushaa@gmail.com>
2018-01-15 12:32:54 +0530

    (oneindiahindi) Improve Logging and Error Handling

--- scrapeNews/scrapeNews/spiders/oneindiaHindi.py
+++ scrapeNews/scrapeNews/spiders/oneindiaHindi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapeNews.items import ScrapenewsItem
-from scrapeNews.pipelines import loggerError
+from scrapeNews.settings import logger
 
 
 class OneindiahindiSpider(scrapy.Spider):
@@ -24,47 +24,66 @@ class OneindiahindiSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse, errback=self.errorRequestHandler)
 
     def errorRequestHandler(self, failure):
-        self.urls_parsed -= 1
-        loggerError.error('Non-200 response at ' + str(failure.request.url))
+        logger.error(__name__ + ' Non-200 response at ' + str(failure.request.url))
 
 
     def parse(self, response):
-        newsContainer = response.xpath('//div[@id="collection-wrapper"]/article')
-        for newsBox in newsContainer:
-            link = 'https://hindi.oneindia.com' + newsBox.xpath('div/h2/a/@href').extract_first()
-            if not self.postgres.checkUrlExists(link):
-                yield scrapy.Request(url=link, callback=self.parse_article, errback=self.errorRequestHandler)
-
+        try:
+            newsContainer = response.xpath('//div[@id="collection-wrapper"]/article')
+            for newsBox in newsContainer:
+                link = 'https://hindi.oneindia.com' + newsBox.xpath('div/h2/a/@href').extract_first()
+                if not self.postgres.checkUrlExists(link):
+                    yield scrapy.Request(url=link, callback=self.parse_article, errback=self.errorRequestHandler)
+                    self.urls_parsed += 1
+                else:
+                    self.urls_dropped += 1
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] " + str(e) + " for response url " + response.url)
 
     def parse_article(self, response):
-        item = ScrapenewsItem()  # Scraper Items
-        item['image'] = self.getPageImage(response)
-        item['title'] = self.getPageTitle(response)
-        item['content'] = self.getPageContent(response)
-        item['newsDate'] = self.getPageDate(response)
-        item['link'] = response.url
-        item['source'] = 110
-        if item['title'] is not 'Error' and item['content'] is not 'Error' and item['newsDate'] is not 'Error':
-            self.urls_scraped += 1
-            yield item
+        try:
+            item = ScrapenewsItem()  # Scraper Items
+            item['image'] = self.getPageImage(response)
+            item['title'] = self.getPageTitle(response)
+            item['content'] = self.getPageContent(response)
+            item['newsDate'] = self.getPageDate(response)
+            item['link'] = response.url
+            item['source'] = 110
+            if item['title'] is not 'Error' and item['content'] is not 'Error' and item['newsDate'] is not 'Error':
+                self.urls_scraped += 1
+                yield item
+            else:
+                self.urls_dropped += 1
+                yield None
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] " + str(e) + " for response url " + response.url)
+            self.urls_dropped += 1
 
 
     def getPageContent(self, response):
-        data = ' '.join(response.xpath("//div[contains(@class,'io-article-body')]//text()").extract())
-        if not data:
-            data = ' '.join(response.xpath("//div[contains(@id,'slider0')]/p/text()").extract())
-        if not data:
-            data = response.xpath("//article//*[not(self::script) and not(self::style)]/text()").extract()
-            data = ' '.join([x for x in data if x != ' ' and x!=u'\xa0']) # Removing all the blank spaces & joining list
-        if not data:
-            loggerError.error(response.url)
+        try:
+            data = ' '.join(response.xpath("//div[contains(@class,'io-article-body')]//text()").extract())
+            if not data:
+                data = ' '.join(response.xpath("//div[contains(@id,'slider0')]/p/text()").extract())
+            if not data:
+                data = response.xpath("//article//*[not(self::script) and not(self::style)]/text()").extract()
+                data = ' '.join([x for x in data if x != ' ' and x!=u'\xa0']) # Removing all the blank spaces & joining list
+            if not data:
+                logger.error(__name__ + " Unable to extract Content : " + response.url)
+                data = 'Error'
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] Unable to extract Content : " + str(e) + " : " + response.url)
             data = 'Error'
         return data
 
     def getPageTitle(self, response):
-        data = response.xpath("//h1[contains(@class,'heading')]/text()").extract_first()
-        if (data is None):
-            loggerError.error(response.url)
+        try:
+            data = response.xpath("//h1[contains(@class,'heading')]/text()").extract_first()
+            if (data is None):
+                logger.error(__name__ + " Unable to Extract Title : " + response.url)
+                data = 'Error'
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] Unable to Extract Title : " + str(e) + " : " + response.url)
             data = 'Error'
         return data
 
@@ -78,7 +97,7 @@ class OneindiahindiSpider(scrapy.Spider):
                     try:
                         data = 'https://hindi.oneindia.com' + response.xpath("//img[contains(@class,'image_listical')]/@src").extract_first()
                     except Exception as Error:
-                        loggerError.error(str(Error) +' occured at: '+ response.url)
+                        logger.error(__name__ + " [UNHANDLED] Unable to Extract Image : " + str(Error) + ' : '+ response.url)
                         data = 'Error'
         return data
 
@@ -86,7 +105,6 @@ class OneindiahindiSpider(scrapy.Spider):
         try:
             data = (response.xpath("//time/@datetime").extract_first()).rsplit('+',1)[0]
         except Exception as Error:
-            loggerError.error(str(Error) + ' occured at: ' + response.url)
+            logger.error(__name__ + " [UNHANDLED] Unable to extract Date : " + str(Error) + ' : ' + response.url)
             data = 'Error'
-        finally:
-            return data
+        return data
