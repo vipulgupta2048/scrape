41a8de93e8ac2e7eb54c4e6f5ce75e1a4ee4270e
Ayush Agarwal <thisisayushaa@gmail.com>
2018-01-15 12:24:32 +0530

    (oneindia) Improve Logging and Error Handling

--- scrapeNews/scrapeNews/spiders/oneindia.py
+++ scrapeNews/scrapeNews/spiders/oneindia.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapeNews.items import ScrapenewsItem
-from scrapeNews.pipelines import loggerError
+from scrapeNews.settings import logger
 
 
 class OneindiaSpider(scrapy.Spider):
@@ -25,46 +25,65 @@ class OneindiaSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse, errback=self.errorRequestHandler)
 
     def errorRequestHandler(self, failure):
-        self.urls_parsed -= 1
-        loggerError.error('Non-200 response at ' + str(failure.request.url))
+        logger.error(__name__ + ' NON-200 response at ' + str(failure.request.url))
 
     def parse(self, response):
-        newsContainer = response.xpath('//div[@id="collection-wrapper"]/article')
-        for newsBox in newsContainer:
-            link = 'https://www.oneindia.com' + newsBox.xpath('div/h2/a/@href').extract_first()
-            if not self.postgres.checkUrlExists(link):
-                yield scrapy.Request(url=link, callback=self.parse_article, errback=self.errorRequestHandler)
-
+        try:
+            newsContainer = response.xpath('//div[@id="collection-wrapper"]/article')
+            for newsBox in newsContainer:
+                link = 'https://www.oneindia.com' + newsBox.xpath('div/h2/a/@href').extract_first()
+                if not self.postgres.checkUrlExists(link):
+                    self.urls_parsed += 1
+                    yield scrapy.Request(url=link, callback=self.parse_article, errback=self.errorRequestHandler)
+                else:
+                    self.urls_dropped += 1
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] " + str(e) + " for response url " + response.url)
 
     def parse_article(self, response):
-        item = ScrapenewsItem()  # Scraper Items
-        item['image'] = self.getPageImage(response)
-        item['title'] = self.getPageTitle(response)
-        item['content'] = self.getPageContent(response)
-        item['newsDate'] = self.getPageDate(response)
-        item['link'] = response.url
-        item['source'] = 109
-        if item['title'] is not 'Error' and item['content'] is not 'Error' and item['newsDate'] is not 'Error':
-            self.urls_scraped += 1
-            yield item
+        try:
+            item = ScrapenewsItem()  # Scraper Items
+            item['image'] = self.getPageImage(response)
+            item['title'] = self.getPageTitle(response)
+            item['content'] = self.getPageContent(response)
+            item['newsDate'] = self.getPageDate(response)
+            item['link'] = response.url
+            item['source'] = 109
+            if item['title'] is not 'Error' and item['content'] is not 'Error' and item['newsDate'] is not 'Error':
+                self.urls_scraped += 1
+                yield item
+            else:
+                self.urls_dropped += 1
+                yield None
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] " + str(e) + " for response url " +response.url)
+            self.urls_dropped += 1
 
 
     def getPageContent(self, response):
-        data = ' '.join(response.xpath("//div[contains(@class,'io-article-body')]/p/text()").extract())
-        if not data:
-            data = response.xpath("//meta[@property='og:description']/@content").extract_first()
-        if data is None:
-            loggerError.error(str(Error) + ' occured at: ' + response.url)
+        try:
+            data = ' '.join(response.xpath("//div[contains(@class,'io-article-body')]/p/text()").extract())
+            if not data:
+                data = response.xpath("//meta[@property='og:description']/@content").extract_first()
+            if data is None:
+                logger.error(__name__ + " Unable to Extract Content : " + response.url)
+                data = 'Error'
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] Unable to Extract Content : " + str(e) + " : " + response.url)
             data = 'Error'
         return data
 
 
     def getPageTitle(self, response):
-        data = response.xpath("//h1[contains(@class,'heading')]/text()").extract_first()
-        if (data is None):
-            data = response.xpath("//meta[@property='og:title']/@content").extract_first()
-        if (data is None):
-            loggerError.error(response.url)
+        try:
+            data = response.xpath("//h1[contains(@class,'heading')]/text()").extract_first()
+            if (data is None):
+                data = response.xpath("//meta[@property='og:title']/@content").extract_first()
+            if (data is None):
+                logger.error(__name__ + " Unable to Extract Title : " + response.url)
+                data = 'Error'
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] Unable to Extract Title : " + str(e) + " : " + response.url)
             data = 'Error'
         return data
 
@@ -80,7 +99,7 @@ class OneindiaSpider(scrapy.Spider):
                 if not data:
                     data = response.xpath("//div[@class='assigned_video']/img/@src").extract_first()
                 if not data:
-                    loggerError.error(str(Error) + " occured at: " + response.url)
+                    logger.error(__name__ + " [UNHANDLED] Unable to Extract Image : " + str(Error) + " : " + response.url)
                     data = 'Error'
         return data
 
@@ -89,7 +108,6 @@ class OneindiaSpider(scrapy.Spider):
         try:
             data = (response.xpath("/html/head/meta[@property='article:published_time']/@content").extract_first()).rsplit('+',1)[0]
         except Exception as Error:
-            loggerError.error(str(Error) + ' occured at: ' + response.url)
+            logger.error(__name__ + " [UNHANDLED] Unable to Extract Date : " + str(Error) + ' : ' + response.url)
             data = 'Error'
-        finally:
-            return data
+        return data
