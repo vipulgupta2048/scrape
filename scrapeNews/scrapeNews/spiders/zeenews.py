661be2b4c51264b5b6017e8d3a2c56504b4bb0f1
Ayush Agarwal <thisisayushaa@gmail.com>
2018-01-18 12:04:13 +0530

    (zeenews) Improve Logging and Error Handling

--- scrapeNews/scrapeNews/spiders/zeenews.py
+++ scrapeNews/scrapeNews/spiders/zeenews.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapeNews.items import ScrapenewsItem
-from scrapeNews.pipelines import loggerError
+from scrapeNews.settings import logger
 from datetime import datetime
 
 
@@ -13,61 +13,91 @@ class zeespider(scrapy.Spider):
         'site_name':'Zee News',
         'site_url':'http://zeenews.india.com/india'}
 
-    def closed(self, reason):
-        self.postgres.closeConnection(reason)
-
     #Scraping the main page for article links
     def parse(self, response):
-        articles = response.xpath('//section[contains(@class, "maincontent")]//div[contains(@class, "section-article")]') #extracts HTML from the start_url
-        #Scrapes columns of articles
-        for article in articles:
-            x = article.xpath('.//h3/a[2]') #extracts <a> tag from start _url
-            link = x.xpath('.//@href').extract_first()  #extracts URL for the articles recursively
-            yield response.follow(link, callback = self.parse_news)
-        
-        #For scraping the links on the next page of the website
-        next_page = response.xpath('//link[@rel = "next"]/@href').extract_first()
-        if next_page is not None:
-           print("==============Switching Page======================")
-           yield response.follow(next_page, callback = self.parse)
+        try:
+            articles = response.xpath('//section[contains(@class, "maincontent")]//div[contains(@class, "section-article")]') #extracts HTML from the start_url
+            #Scrapes columns of articles
+            for article in articles:
+                x = article.xpath('.//h3/a[2]') #extracts <a> tag from start _url
+                link = x.xpath('.//@href').extract_first()  #extracts URL for the articles recursively
+                self.urls_parsed += 1
+                yield response.follow(link, callback = self.parse_news)
+            
+            #For scraping the links on the next page of the website
+            next_page = response.xpath('//link[@rel = "next"]/@href').extract_first()
+            if next_page is not None:
+            logger.info(__name__ + " ==============Switching Page======================")
+            yield response.follow(next_page, callback = self.parse)
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] " + str(e) + " for response url " + response.url)
 
     #For scraping a particular article listed on the main page
     def parse_news(self,response):
-        i = ScrapenewsItem()
-        i['title'] =  self.gettitle(response)   #scrapes headline
-        i['newsDate'] = self.getdatetime(response)  #scrapes date
-        i['image'] = self.getimage(response)    #scrapes image link
-        i['content'] = self.getcontent(response)    #scrapes content
-        i['link'] = response.url    #scrapes link 
-        i['source'] = 106   #database entry
+        try:
+            i = ScrapenewsItem()
+            i['title'] =  self.gettitle(response)   #scrapes headline
+            i['newsDate'] = self.getdatetime(response)  #scrapes date
+            i['image'] = self.getimage(response)    #scrapes image link
+            i['content'] = self.getcontent(response)    #scrapes content
+            i['link'] = response.url    #scrapes link 
+            i['source'] = 106   #database entry
 
-        yield i
+            flag = 0
+            for x in i:
+                if i[x] == 'Error':
+                    self.urls_dropped += 1
+                    flag = 1
+                    yield None
+                    break
+            if flag == 0:
+                self.urls_scraped += 1
+                yield i
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] " + str(e) + " for response url " + response.url)
+            yield None
          
     def gettitle(self,response):
-        data = response.xpath('//h1[contains(@class, "article-heading margin")]/text()').extract_first() 
-        if (data is None):
-            loggerError.error(response.url)
-            data = 'Error - No title found'
+        try:
+            data = response.xpath('//h1[contains(@class, "article-heading margin")]/text()').extract_first() 
+            if (data is None):
+                logger.error(__name__ + " Unable to Extract Title : " + response.url)
+                data = 'Error'
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] Unable to Extract Title : " + str(e) + " : " + response.url)
+            data = 'Error'
         return data   
 
     def getdatetime(self,response):
-        data = response.xpath('//span[contains(@class, "date")]/text()').extract_first()[10:-4] 
-        z = (datetime.strptime(data," %b %d, %Y, %H:%M %p")).strftime("%Y-%m-%d %H:%M:%S")  
-        if (z is None):
-            loggerError.error(response.url)
-            data = 'Error - date format wrong'
+        try:
+            data = response.xpath('//span[contains(@class, "date")]/text()').extract_first()[10:-4] 
+            z = (datetime.strptime(data," %b %d, %Y, %H:%M %p")).strftime("%Y-%m-%d %H:%M:%S")  
+            if (z is None):
+                logger.error(__name__ + " Unable to Extract Title : " + response.url)
+                data = 'Error'
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] Unable to Extract Date : " + str(e) + " : " + response.url)
+            data = 'Error'
         return data   
 
     def getcontent(self,response):
-        data = response.xpath('//div[contains(@class, "article")]/div[contains(@class, "field")]//p/text()').extract()
-        if (data is None):
-            loggerError.error(response.url)
-            data = 'Error - No content found'
+        try:
+            data = response.xpath('//div[contains(@class, "article")]/div[contains(@class, "field")]//p/text()').extract()
+            if (data is None):
+                logger.error(__name__ + " [UNHANDLED] Unable to Extract Title : " + response.url)
+                data = 'Error'
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] Unable to Extract Content : " + str(e) + " : " + response.url)
+            data = 'Error'
         return data
 
     def getimage(self,response):
-        data = response.xpath('//div[contains(@class, "field-item")]/img/@src').extract_first() #scrapes image url
-        if (data is None):
-            loggerError.error(response.url)
-            data = 'Error - No image found'
+        try:
+            data = response.xpath('//div[contains(@class, "field-item")]/img/@src').extract_first() #scrapes image url
+            if (data is None):
+                logger.error(__name__ + " Unable to Extract Image : " + str(e) + " : " + response.url)
+                data = 'Error'
+        except Exception as e:
+            logger.error(__name__ + " [UNHANDLED] Unable to Extract Image : " + str(e) + " : " + response.url)
+            data = 'Error'
         return data
